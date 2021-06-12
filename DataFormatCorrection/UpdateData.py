d235f4f513ada95a5a6e07a355c0ea0d45d81ae0
hekkaaa <silencemyalise@gmail.com>
2021-06-12 23:28:02 +0600

    edit

--- DataFormatCorrection/UpdateData.py
+++ DataFormatCorrection/UpdateData.py
@@ -5,20 +5,28 @@ import time
 import requests
 from DataFormatCorrection.UpdateData import *
 
-#проводим время из Азорских островов к Москве
+
+# проводим время из Азорских островов к Москве
 def updateloghourse(datafromserver):
     upgradedata = datetime.datetime.strptime(datafromserver, "%Y-%m-%d %H:%M:%S")
     upgradedata = upgradedata + datetime.timedelta(hours=3)
     return upgradedata
 
+#Преобразование str в datetime.datetime
+def update_str_for_datatime(datafromserver):
+    upgradedata = datetime.datetime.strptime(datafromserver, "%Y-%m-%d %H:%M:%S")
+    return upgradedata
+
+#Преобразование текущей даты для названия таблицы БД.
 def realdatatame():
     t = datetime.datetime.date(datetime.datetime.now())
-    #t = t + datetime.timedelta(hours=48) #Увеличение\уменьшение времени
+    # t = t + datetime.timedelta(hours=48) #Увеличение\уменьшение времени
     t = t.strftime('%d_%m_%Y')
     return t
 
+
 ####
-def firststartreturnhistoryTrade_sql(response,logdatanametech):
+def firststartreturnhistoryTrade_sql(response, logdatanametech, table_name):
     # Время торгов идет по Азорским остравам.
     requestJSON = ''.join(map(str, response.json()))  # пребразование list в str для нормального чтения в лог.
     outserverlocallog = "GET \nServer status OK\nJSON: " + requestJSON + '\n' + '_' * 60
@@ -42,7 +50,7 @@ def firststartreturnhistoryTrade_sql(response,logdatanametech):
         sql_json.append(workrequest[i]['amount'])
         sql_json.append(workrequest[i]['total'])
 
-        sqlADDinfoTable(sql_json)
+        sqlADDinfoTable(sql_json,table_name)
         sql_json.clear()
 
         if (i == 199):
@@ -53,7 +61,7 @@ def firststartreturnhistoryTrade_sql(response,logdatanametech):
     return lastdatetimetrade
 
 
-def cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech):
+def cycleupdatelogmarket_sql(lastdatetimetrade, params, logdatanametech, name_table):
     response = requests.get('https://poloniex.com/public', params=params)
     if (response.status_code):
         requestJSON = ''.join(map(str, response.json()))  # пребразование list в str для нормального чтения.
@@ -67,12 +75,12 @@ def cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech):
         workrequest = response.json()
         workrequest.reverse()
         if (updateloghourse(workrequest[-1]['date']) == lastdatetimetrade):
-            #Если время совпало с последней записью 200 сделок то - возвращаем последнюю дату сделки без изменения
+            # Если время совпало с последней записью 200 сделок то - возвращаем последнюю дату сделки без изменения
             print('Время совпало')
             return lastdatetimetrade
         else:
-            #Если время последней сделки НЕ совпало - заносим инфу в БД и переменную lastdatetimetrade
-            result = ''
+            # Если время последней сделки НЕ совпало - заносим инфу в БД и переменную lastdatetimetrade
+
             print('время не совпало')
             sql_json = []
             sql_json.append(workrequest[-1]['globalTradeID'])
@@ -83,7 +91,7 @@ def cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech):
             sql_json.append(workrequest[-1]['amount'])
             sql_json.append(workrequest[-1]['total'])
 
-            sqlADDinfoTable(sql_json)
+            sqlADDinfoTable(sql_json, name_table)
             sql_json.clear()
 
             # обновление последней даты сделки в переменной lastdatetimetrade для следующего цикла
@@ -92,8 +100,8 @@ def cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech):
             return lastdatetimetrade
 
 
-#Запись в подготовленную таблицу sql
-def sqlADDinfoTable(addinfo):
+# Запись в подготовленную таблицу sql
+def sqlADDinfoTable(addinfo, name_table):
     try:
         # Подключение к существующей базе данных
         connection = psycopg2.connect(user="postgres",
@@ -104,23 +112,117 @@ def sqlADDinfoTable(addinfo):
                                       database="postgres")
 
         cursor = connection.cursor()
-        print(addinfo)
         # Выполнение SQL-запроса для вставки данных в таблицу
-        insert_query = f""" INSERT INTO postgreetest_db (globalTradeID, tradeID, date, type, rate, amount, total) VALUES
+        insert_query = f""" INSERT INTO {name_table} (globalTradeID, tradeID, date, type, rate, amount, total) VALUES
             (%s,%s,%s,%s,%s,%s,%s)"""
 
-        cursor.execute(insert_query,addinfo)
+        cursor.execute(insert_query, addinfo)
         connection.commit()
         print("запись успешно вставлена")
-
-        # Получить результат
-        cursor.execute("SELECT * from postgreetest_db")
-        record = cursor.fetchall()
-        print("Результат", record)
 
     except (Exception, Error) as error:
         print("Ошибка при работе с PostgreSQL", error)
 
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+# Функция выплевывает список таблиц в базе данных.
+def chectablefromDB(name_table):
+    try:
+        # Подключение к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password="111111",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres")
+        cursor = connection.cursor()
+        # Выполнение SQL-запроса для вставки данных в таблицу
+        insert_query = """SELECT table_name FROM information_schema.tables
+    WHERE table_schema NOT IN ('information_schema', 'postgres')
+    AND table_schema IN('public', 'myschema')"""
+
+        # Получить результат
+        cursor.execute(insert_query)
+        record = cursor.fetchall()
+        for a in record:
+            if (a[0] == name_table):
+                return True
+            else:
+                pass
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+
+# Создание новой таблицы с именем дня трейда и валюты.
+def create_table_sql(name_table):
+    try:
+        # Подключение к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password="111111",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres")
+
+        cursor = connection.cursor()
+        # Выполнение SQL-запроса для вставки данных в таблицу
+        insert_query = f"""CREATE TABLE {name_table} (
+        globaltradeid         int8,
+        tradeid        int8,
+        date 			varchar,
+        type		varchar,
+        rate 		float4,
+        amount		float4,
+        total		float4
+    )"""
+
+        cursor.execute(insert_query)  # Создание таблицы
+        connection.commit()
+        print(f"Таблица успешно {name_table} создана")
+
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+
+#Последняя запись в таблице трейдинга
+def check_last_date_edit_table(name_table):
+    try:
+        # Подключение к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password="111111",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres")
+
+        cursor = connection.cursor()
+        # Выполнение SQL-запроса для вставки данных в таблицу
+        insert_query = f"""SELECT date FROM {name_table} WHERE date=(SELECT max(date) FROM {name_table});"""
+
+        cursor.execute(insert_query)  # Создание таблицы
+        record = cursor.fetchone()
+        return record[0]
+
+
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
     finally:
         if connection:
             cursor.close()
--- main.py
+++ main.py
@@ -4,39 +4,44 @@ import os.path
 from DataFormatCorrection.UpdateData import *
 
 #header
+cryptomoney = 'BTC_ETH'
 params = (
     ('command', 'returnTradeHistory'),
-    ('currencyPair', 'BTC_ETH'),
+    ('currencyPair', cryptomoney),
 )
 ############
 
-check_file = os.path.exists(f'TradeHistory{realdatatame()}.txt')
-logdatanamemarket = f'TradeHistory{realdatatame()}.txt'
+name_table = f'tradehistory_{cryptomoney}_{realdatatame()}'
+name_table = name_table.lower()
+logdatanamemarket = f'tradehstory{realdatatame()}.txt'
 logdatanametech = f'techlog{realdatatame()}.txt'
 
-if check_file:
+check_table_db = chectablefromDB(name_table)
+
+if check_table_db:
     print('file here!')
     # дополнение лога в бесконечном цикле.
-    i = 0
-    lastdatetimetrade = ''
-    while i < 999:
-        time.sleep(10)
-        pass
-        # lastdatetimetrade = cycleupdatelogmarket(lastdatetimetrade, params,logdatanamemarket,logdatanametech)
-    print('SCRIPT DONE')
+    lastdatetimetrade = updateloghourse(check_last_date_edit_table(name_table))
+    while True:
+        lastdatetimetrade = cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech,name_table)
+        time.sleep(2)
+
 else:
     # marketlog = open(logdatanamemarket, 'w')
     # marketlog.close()
 
     response = requests.get('https://poloniex.com/public', params=params)
     if (response.status_code):
-        lastdatetimetrade = firststartreturnhistoryTrade_sql(response,logdatanametech)
+        #Создание новой таблицы
+        create_table_sql(name_table)
+        #Первичное наполнение таблицы
+        lastdatetimetrade = firststartreturnhistoryTrade_sql(response,logdatanametech, name_table)
 
         # дополнение лога в бесконечном цикле.
         i = 0
-        while i < 999:
+        while True:
             time.sleep(2)
-            lastdatetimetrade = cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech)
+            lastdatetimetrade = cycleupdatelogmarket_sql(lastdatetimetrade, params,logdatanametech,name_table)
 
         print('SCRIPT DONE')
     else:
@@ -49,6 +54,7 @@ else:
         techlog = open(logdatanametech, 'w')
         techlog.write(outserverlocallog)
         techlog.close()
+
 
 
 
