1df0ac7df5ac52896000932a5bccb09682849a0e
hekkaaa <silencemyalise@gmail.com>
2021-06-20 19:41:08 +0300

    Вполне работающую версия на локильном компе.

---
+++ static/js/autchscript.js
@@ -0,0 +1,16 @@
+console.log('JS OK')
+
+async function test_response(){
+    let url = 'http://127.0.0.1:5000/get'
+    let response = await fetch(url);
+    let result = await response.json()
+    console.log(result)
+
+     // добавление на html информации
+    let div = document.createElement('div');
+    div.className = "alert";
+    //Вытаскивание из get информации по таблице. Прилетает array
+    div.innerHTML = "<strong>JSON ответ от FLASK!</strong> <br> " + result[0][0];
+    document.body.append(div);
+
+}
